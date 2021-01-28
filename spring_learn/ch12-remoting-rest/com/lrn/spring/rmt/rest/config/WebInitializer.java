8babd264c9f873bf78a24a116eebf54a2aec6013
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-01-28 16:14:09 +0530

    Closes #15

--- spring_learn/ch12-remoting-rest/com/lrn/spring/rmt/rest/config/WebInitializer.java
+++ spring_learn/ch12-remoting-rest/com/lrn/spring/rmt/rest/config/WebInitializer.java
@@ -3,13 +3,14 @@ package com.lrn.spring.rmt.rest.config;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.lrn.spring.rmt.config.DataServiceConfig;
+import com.lrn.spring.rmt.rest.config.security.SecurityConfig;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		// TODO Auto-generated method stub
-		return new Class<?>[] { DataServiceConfig.class };
+		return new Class<?>[] { DataServiceConfig.class, SecurityConfig.class };
 	}
 
 	@Override
---
+++ spring_learn/ch12-remoting-rest/com/lrn/spring/rmt/rest/config/security/SecurityWebAppConfig.java
@@ -0,0 +1,7 @@
+package com.lrn.spring.rmt.rest.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppConfig extends AbstractSecurityWebApplicationInitializer{
+
+}
