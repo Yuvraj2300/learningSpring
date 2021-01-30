9296460eaf63e2ba5ee042829c5cba986b69e171
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-01-30 18:33:39 +0530

    Closes #34

---
+++ spring_learn/ch12-remoting-rest/com/lrn/spring/rmt/rest/config/fortests/ServiceTestConfig.java
@@ -0,0 +1,47 @@
+package com.lrn.spring.rmt.rest.config.fortests;
+
+import javax.sql.DataSource;
+
+import org.dbunit.DataSourceDatabaseTester;
+import org.dbunit.util.fileloader.XlsDataFileLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import com.lrn.spring.rmt.services.DBInitializer;
+
+@Configuration
+@ComponentScan(basePackages = { "" }, excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DBInitializer.class) })
+@Profile("test")
+public class ServiceTestConfig {
+	private static Logger logger = LoggerFactory.getLogger(ServiceTestConfig.class);
+
+	@Bean
+	public DataSource dataSource() {
+		try {
+			EmbeddedDatabaseBuilder dbBuilder = new EmbeddedDatabaseBuilder();
+			return dbBuilder.setType(EmbeddedDatabaseType.H2).build();
+		} catch (Exception e) {
+			logger.error("Embedded DataSource bean cannot be created!", e);
+			return null;
+		}
+	}
+
+	@Bean(name = "databaseTester")
+	public DataSourceDatabaseTester dataSourceDatabaseTester() {
+		DataSourceDatabaseTester databaseTester = new DataSourceDatabaseTester(dataSource());
+		return databaseTester;
+	}
+
+	@Bean(name = "xlsDataFileLoader")
+	public XlsDataFileLoader xlsDataFileLoader() {
+		return new XlsDataFileLoader();
+	}
+}
