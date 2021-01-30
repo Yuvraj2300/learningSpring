1b2dadda83b0b22c757e666cf08a37124f7d27f9
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-01-30 13:53:11 +0530

    Closes #28

--- spring_learn/src/test/java/com/lrn/spring/chp13/rest/SingerControllerTest.java
+++ spring_learn/src/test/java/com/lrn/spring/chp13/rest/SingerControllerTest.java
@@ -1,12 +1,20 @@
 package com.lrn.spring.chp13.rest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.ui.ExtendedModelMap;
 
+import com.lrn.spring.rmt.rest.controller.SingerController;
 import com.lrn.spring.rmt.rest.model.Singer;
+import com.lrn.spring.rmt.rest.model.Singers;
 import com.lrn.spring.rmt.rest.service.SingerService;;
 
 public class SingerControllerTest {
@@ -24,7 +32,18 @@ public class SingerControllerTest {
 
 	@Test
 	public void testList() throws Exception {
-		SingerService singerService;
+		SingerService singerService = mock(SingerService.class);
+		when(singerService.findAll()).thenReturn(singers);
+
+		SingerController cont = new SingerController();
+
+		ReflectionTestUtils.setField(cont, "singerService", singerService);
+		
+		ExtendedModelMap uiModel = new ExtendedModelMap();
+		uiModel.addAttribute("singers", cont.listData());
+
+		Singers modelSingers = (Singers) uiModel.get("singers");
+		assertEquals(1, modelSingers.getSingers().size());
 	}
 
 }
