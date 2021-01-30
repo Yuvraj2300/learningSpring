b4365b17afbc58f985ab60bd416be4d2ab777449
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-01-30 14:05:36 +0530

    closes #29

--- spring_learn/src/test/java/com/lrn/spring/chp13/rest/SingerControllerTest.java
+++ spring_learn/src/test/java/com/lrn/spring/chp13/rest/SingerControllerTest.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.ui.ExtendedModelMap;
 
@@ -38,12 +40,37 @@ public class SingerControllerTest {
 		SingerController cont = new SingerController();
 
 		ReflectionTestUtils.setField(cont, "singerService", singerService);
-		
+
 		ExtendedModelMap uiModel = new ExtendedModelMap();
 		uiModel.addAttribute("singers", cont.listData());
 
 		Singers modelSingers = (Singers) uiModel.get("singers");
 		assertEquals(1, modelSingers.getSingers().size());
+	}
+
+	@Test
+	public void testCreate() {
+		final Singer newSinger = new Singer();
+		newSinger.setId(999l);
+		newSinger.setFirstName("BB");
+		newSinger.setLastName("King");
+
+		SingerService singerService = mock(SingerService.class);
+		when(singerService.save(newSinger)).thenAnswer(new Answer<Singer>() {
+			public Singer answer(InvocationOnMock invocation) throws Throwable {
+				singers.add(newSinger);
+				return newSinger;
+			}
+		});
+
+		SingerController cont = new SingerController();
+		ReflectionTestUtils.setField(cont, "singerService", singerService);
+
+		Singer singer = cont.create(newSinger);
+		assertEquals(Long.valueOf(999l), singer.getId());
+		assertEquals("BB", singer.getFirstName());
+
+		assertEquals(2, singers.size());
 	}
 
 }
