91d08b54624767e24c841fc90c7ecfb9dda5cb4b
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-02-02 23:17:19 +0530

    Closes #39

--- spring_learn/ch16-webapps/com/lrn/spring/web/entities/Singer.java
+++ spring_learn/ch16-webapps/com/lrn/spring/web/entities/Singer.java
@@ -1,19 +1,64 @@
 package com.lrn.spring.web.entities;
 
 import java.io.Serializable;
-import java.sql.Blob;
+import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "singer")
 public class Singer implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	private Long id;
+
+	@Version
+	@Column(name = "VERSION")
 	private int version;
+
+	@NotEmpty(message = "{validation.firstname.NotEmpty.message}")
+	@Size(min = 3, max = 60, message = "{validation.firstname.Size.message}")
+	@Column(name = "FIRST_NAME")
 	private String firstName;
+
+	@NotEmpty(message = "{validation.lastname.NotEmpty.message}")
+	@Size(min = 3, max = 60, message = "{validation.lastname.Size.message}")
+	@Column(name = "LAST_NAME")
 	private String lastName;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "BIRTH_DATE")
+	private Date birthDate;
+
+	@Column(name = "DESCRIPTION")
 	private String description;
+
+	@Basic(fetch = FetchType.LAZY)
+	@Lob
+	@Column(name = "photo")
 	private byte photo;
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
 
 	public Long getId() {
 		return id;
