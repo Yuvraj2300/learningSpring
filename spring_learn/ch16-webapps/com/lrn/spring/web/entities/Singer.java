f986a2a37033c01b08c6f3ef057ad2b76771bf74
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-02-02 22:28:48 +0530

    Working on #37

--- spring_learn/ch16-webapps/com/lrn/spring/web/entities/Singer.java
+++ spring_learn/ch16-webapps/com/lrn/spring/web/entities/Singer.java
@@ -14,4 +14,59 @@ public class Singer implements Serializable {
 	private String lastName;
 	private String description;
 	private byte photo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public byte getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte photo) {
+		this.photo = photo;
+	}
+
+	@Override
+	public String toString() {
+		return "Singer [id=" + id + ", version=" + version + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", description=" + description + ", photo=" + photo + "]";
+	}
+
 }
