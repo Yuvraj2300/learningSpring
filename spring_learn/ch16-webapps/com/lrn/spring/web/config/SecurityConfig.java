377ae5a7954cb9ab0e8fcd324fc6d6add7f140aa
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-02-05 15:34:58 +0530

    Closes #47 and working on #46

---
+++ spring_learn/ch16-webapps/com/lrn/spring/web/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.lrn.spring.web.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityConfig extends AbstractSecurityWebApplicationInitializer {
+
+}
