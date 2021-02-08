ff8dba8722f864031b208b721e976cee6cb6356b
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-02-08 12:44:55 +0530

    Closes #48

--- spring_learn/ch16-webapps/com/lrn/spring/web/config/WebAppConfig.java
+++ spring_learn/ch16-webapps/com/lrn/spring/web/config/WebAppConfig.java
@@ -1,5 +1,41 @@
 package com.lrn.spring.web.config;
 
-public class WebAppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.lrn.spring.web" })
+public class WebAppConfig implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/").setCachePeriod(31556926);
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("/WEB-INF/views");
+		vr.setSuffix(".jspx");
+		vr.setRequestContextAttribute("requestContext");
+		return vr;
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("singers/list");
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 
 }
