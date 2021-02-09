0d2b143e2e0cd9cb3cf064538b3a1386acf3af66
ys19299 <ys19299@DE-YS19299-01.zs.local>
2021-02-09 13:00:15 +0530

    Closes #50 & Closes #51

---
+++ spring_learn/ch16-webapps/com/lrn/spring/web/controller/SingerController.java
@@ -0,0 +1,39 @@
+package com.lrn.spring.web.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lrn.spring.web.entities.Singer;
+import com.lrn.spring.web.service.SingerService;
+
+@RequestMapping("/singers")
+@Controller
+public class SingerController {
+	private final Logger logger = LoggerFactory.getLogger(SingerController.class);
+
+	private SingerService singerService;
+	// private MessageSource messageSource;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Model model) {
+		logger.info("Listing singers");
+		List<Singer> singers = singerService.findAll();
+		model.addAttribute("singers", singers);
+		return "/singers/list";
+	}
+
+	@Autowired
+	public SingerController(SingerService singerService) {
+		super();
+		this.singerService = singerService;
+	}
+
+}
